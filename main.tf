5793d340461ccf8ea3fbf04e262bf0a9dc998cd2
Yukon <lorraine-c98@outlook.com>
2025-10-24 20:05:00 +0800

    Var concept introduced, let terraform file reusable

--- main.tf
+++ main.tf
@@ -5,13 +5,24 @@ provider "aws" {
 
 }
 
+# adding params for terraform
+variable "cider_blocks" {
+    description = "cidr blocks for vpc and subnets"
+    default = "10.0.10.0/24" # if no params, will set value as the default
+    type = list(object({
+      name = string
+      cidr_block = string
+    })) # specify what value types are accepted 
+}
+
+
 # what we can do with the provider
 # working with aws resources
 resource "aws_vpc" "dev-vpc" {
     # each block describes one or more infra objects
-    cidr_block = "10.0.0.0/16" # 按需控制ip数量, subnets calculator will help with this
+    cidr_block = var.cider_blocks[0].cider_block # 按需控制ip数量, subnets calculator will help with this
     tags = {
-      Name = "development",
+      Name = var.cider_blocks[0].name
       vpc_env = "dev"
     }
   
@@ -19,10 +30,10 @@ resource "aws_vpc" "dev-vpc" {
 
 resource "aws_subnet" "dev-subnet-1" {
     vpc_id = aws_vpc.dev-vpc.id # Declarative and coding feature 
-    cidr_block = "10.0.10.0/24"
+    cidr_block = var.cider_blocks[1].cider_block
     availability_zone = "ap-southeast-1a"
     tags = {
-      Name = "subnet-1-dev"
+      Name = var.cider_blocks[1].name
     
     }
   
@@ -31,12 +42,14 @@ resource "aws_subnet" "dev-subnet-1" {
 # create a new resource based on the existing one
 data "aws_vpc" "existing" {
   # query the existing resources and components from AWS 
-  # resource name + query_result name
+  # resource name + query_result name to expose this result 
+
+  # query conditions: 
   default = true
 }
 
 resource "aws_subnet" "dev-subnet-2" {
-    vpc_id = data.aws_vpc.existing.id
+    vpc_id = data.aws_vpc.existing.id # query and then use it right here
     cidr_block = "172.31.48.0/20"
     availability_zone = "ap-southeast-1a"
     tags = {
