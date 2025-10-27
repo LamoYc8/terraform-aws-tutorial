af696b46e908ca36fd42a62d6515d2cfeaa5f2da
Yukon <lorraine-c98@outlook.com>
2025-10-27 18:23:48 +0800

    ec2 instance created with key pair attached

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ resource "aws_security_group" "myapp_sg" {
 # latest practice: one CIDR block per rule
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   security_group_id = aws_security_group.myapp_sg.id 
-  cidr_ipv4 = var.my_ip
+  cidr_ipv4 = var.my_ip # ssh will not use proxy, so set it using original ip
   ip_protocol = "tcp"
   from_port = 22 # can be a range, if only needs one, then setting from and to the same value
   to_port = 22
@@ -102,10 +102,47 @@ resource "aws_vpc_security_group_ingress_rule" "allow_nginx" {
 }
 
 
-resource "aws_vpc_security_group_egress_rule" "name" {
+resource "aws_vpc_security_group_egress_rule" "allow_all_egress" {
   security_group_id = aws_security_group.myapp_sg.id
   cidr_ipv4 = "0.0.0.0/0"
   ip_protocol = "-1" # no limitation for protocol
   # semantically equivalent to all ports
   
+}
+
+# instance section 
+
+# fetch ami inform using data source 
+data "aws_ami" "latest_amazon_linux" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
+
+resource "aws_instance" "myapp_ec2" {
+  # 2 required args
+  ami = data.aws_ami.latest_amazon_linux.id  
+  instance_type = "t2.micro" # based on how much resources we need
+
+  # optional args
+  tags = {
+    Name = "${var.env_prefix}-server"
+  }
+
+  subnet_id = aws_subnet.myapp_subnet_1.id 
+  vpc_security_group_ids = [aws_security_group.myapp_sg.id]
+  availability_zone = var.avail_zone
+
+  associate_public_ip_address = true # a public ip address to access from the Internet
+  key_name = "server-key-pair" # using existing key pair to ssh to the server 
 }
